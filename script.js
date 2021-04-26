c679fd8b517d8cf0168551f4498d3a2b5838bde0
yubaEloualidi2 <69818198+yubzia@users.noreply.github.com>
2021-04-26 23:34:13 +0200

    upload Files

---
+++ script.js
@@ -0,0 +1,15 @@
+var result = document.getElementById("result");
+var play = document.getElementById("play");
+var remove = document.getElementById("remove");
+var editor = document.getElementById("editor");
+
+play.onclick = ()=>{
+    result.innerHTML = editor.value;
+}
+
+remove.onclick = ()=>{
+    result.innerHTML = "";
+}
+onload = ()=>{
+    editor.value = localStorage.getItem("Codes");
+}
